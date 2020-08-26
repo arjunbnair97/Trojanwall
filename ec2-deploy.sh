807e0b22e448618d5c6955d494ab157a347eeaf1
arjunbnair97 <53293413+arjunbnair97@users.noreply.github.com>
2020-08-26 23:19:49 +0530

    Create ec2-deploy.sh

---
+++ ec2-deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+set -f
+ssh ubuntu@$PROD_DEPLOY_SERVER "
+sudo chmod 777 -R /var/www/html && 
+cd / && 
+cd /var/www/html && 
+git init && 
+git remote add origin https://gitlab.com/arbnair97/trojanwallg2.git && 
+git checkout -b Production && 
+git pull https://gitlab.com/arbnair97/trojanwallg2.git Production"
+
