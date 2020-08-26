eae8dd3c9184b60cf778821d2f8e619506d455c5
arjunbnair97 <53293413+arjunbnair97@users.noreply.github.com>
2020-08-27 00:38:09 +0530

    Update ec2-deploy.sh

--- ec2-deploy.sh
+++ ec2-deploy.sh
@@ -7,7 +7,7 @@ sudo chmod 777 -R /var/www/html &&
 cd / && 
 cd /var/www/html && 
 git init && 
-git remote add origin https://gitlab.com/arbnair97/trojanwallg2.git && 
+git remote add origin https://gitlab.com/arbnair97/trojanwall.git && 
 git checkout -b Production && 
-git pull https://gitlab.com/arbnair97/trojanwallg2.git Production"
+git pull https://gitlab.com/arbnair97/trojanwall.git Production"
 
