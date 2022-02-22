b7426520f93bd81421cdb9e15afca8a3e3fa1b7c
arjunbnair97 <53293413+arjunbnair97@users.noreply.github.com>
2022-02-22 10:18:59 +0530

    Delete ec2-machine-conf.sh

--- ec2-machine-conf.sh
+++
@@ -1,9 +0,0 @@
-
-#!/bin/bash
-
-
-sudo apt-get update -y          # For updating the ubuntu machine.
-sudo apt-get install apache2 -y # For installing the Apache2 Server in the machine.
-
-sudo systemctl enable apache2 # For enabling the Apache2 service.
-sudo systemctl start apache2  # For starting the service in the machine.
